407b11fa1c5432d67065849ecd9e755c134bc08f
Bhavya G. <anim8tor.ing@gmail.com>
2025-11-15 13:28:11 +0530

    prj setup

---
+++ src/client/java/com/caiga/CAIGAClient.java
@@ -0,0 +1,10 @@
+package com.caiga;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CAIGAClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
